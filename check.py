39d9eeba5d170e5ed1ee7228d261c56e27354ed3
altm4n <33285681+altm4n@users.noreply.github.com>
2023-03-02 19:05:50 +0800

    Create check.py

---
+++ check.py
@@ -0,0 +1,17 @@
+from config import TE_NAME
+from modules import  SinkDetect, Fuzz, Exploit
+from Logger import logger
+
+if __name__ == "__main__":
+    logger.info("[*]Target TE : " + TE_NAME)
+    sink_detect= SinkDetect.SinkDetect()
+    sink_detect.set_seed_file_name(TE_NAME + "_test.xlsx")
+    sink_detect.detect()
+    fuzz = Fuzz.Fuzz()
+    fuzz.set_sink_file_name(TE_NAME+"_sink.xlsx")
+    fuzz.fuzz()
+    exp = Exploit.Exploit()
+    exp.set_poc_file_name(TE_NAME+"_poc.xlsx")
+    exp_list = exp.code_wrapper()
+    logger.info("[*]Generate "+str(exp_list.__len__())+" EXP")
+    logger.info(exp_list)
